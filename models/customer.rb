e9f464973166e6d54c0a775ec38a85a215f19030
Yoni Satat <yoni.satat@gmail.com>
2017-10-13 13:33:21 +0100

    added initialize method to customer.rb

---
+++ models/customer.rb
@@ -0,0 +1,13 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @funds = options['funds']
+  end
+end
