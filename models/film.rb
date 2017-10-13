df7b1b24a85d286a9eec30b5eb5f17b8b85bbc48
Yoni Satat <yoni.satat@gmail.com>
2017-10-13 14:54:58 +0100

    added update method to film.rb

--- models/film.rb
+++ models/film.rb
@@ -46,4 +46,19 @@ class Film
     values = [@id]
     SqlRunner.run(sql, values)
   end
+
+  def update()
+    sql = "
+        UPDATE films
+        SET
+        (
+          title,
+          price
+        ) = (
+          $1, $2
+        ) WHERE id = $3
+      "
+      values = [@title, @price, @id]
+      SqlRunner.run(sql, values)
+  end
 end
