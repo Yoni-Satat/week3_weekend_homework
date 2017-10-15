6001ad1f29ac9200a73818db0633802e86d1e528
Yoni Satat <yoni.satat@gmail.com>
2017-10-15 13:19:01 +0100

    addded update method to tickects.rb

--- models/tickets.rb
+++ models/tickets.rb
@@ -2,7 +2,7 @@ require_relative("../db/sql_runner")
 
 class Ticket
 
-  attr_reader :id, :customer_id, :film_id
+  attr_accessor :id, :customer_id, :film_id
 
   def initialize(options)
     @id = options['id'].to_i
@@ -44,5 +44,20 @@ class Ticket
     sql = "DELETE FROM tickets WHERE id = $1"
     values = [@id]
     SqlRunner.run(sql, values)
+  end
+
+  def update()
+    sql = "
+        UPDATE tickets
+        SET
+        (
+          customer_id,
+          film_id
+        ) = (
+          $1, $2
+        ) WHERE id = $3
+      "
+      values = [@customer_id, @film_id, @id]
+      SqlRunner.run(sql, values)
   end
 end
